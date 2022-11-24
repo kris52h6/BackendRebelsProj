1bfbb51ec9fbff88247cf5868f7aa49467a71207
alexander6273 <alex6273@stud.kea.dk>
2022-11-24 09:54:26 +0100

    added team og signup

--- src/main/java/dat3/backend/api/TeamController.java
+++ src/main/java/dat3/backend/api/TeamController.java
@@ -1,8 +1,11 @@
 package dat3.backend.api;
 
 import dat3.backend.dto.TeamDTO;
+import dat3.backend.entity.Team;
 import dat3.backend.service.TeamService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -19,5 +22,10 @@ public class TeamController
     @GetMapping("/{teamId}")
     public TeamDTO getTeamById(@PathVariable int teamId){
         return teamService.getTeamById(teamId);
+    }
+
+    @GetMapping()
+    public List<TeamDTO> getAllTeams(){
+        return teamService.getAllTeams();
     }
 }
